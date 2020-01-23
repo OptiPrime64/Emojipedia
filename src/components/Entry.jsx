d868b3490e4792d81acd733a13195c6247e6a444
OptiPrime64 <58306277+OptiPrime64@users.noreply.github.com>
2020-01-23 13:48:40 -0500

    Initial commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Entry(fromApp) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {fromApp.emoji}
+        </span>
+        <span>{fromApp.name}</span>
+      </dt>
+      <dd>{fromApp.meaning}</dd>
+    </div>
+  );
+}
+
+export default Entry;
